a68be50ece824453f4b3fcd92e73eadbdfcfaf69
stanley1210 <stbs1210@gmail.com>
2024-06-28 12:12:28 +0800

    專題

---
+++ src/main/java/com/spring_kajarta_frontstage/SpringKajartaFrontstageApplication.java
@@ -0,0 +1,13 @@
+package com.spring_kajarta_frontstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKajartaFrontstageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKajartaFrontstageApplication.class, args);
+	}
+
+}
