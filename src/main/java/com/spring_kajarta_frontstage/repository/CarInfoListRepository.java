29d4a5189fd4bdca9692106e40d92cf55b3704dc
Vic <g27942110n@gmail.com>
2024-07-01 21:57:23 +0800

    test

--- src/main/java/com/spring_kajarta_frontstage/repository/CarInfoListRepository.java
+++ src/main/java/com/spring_kajarta_frontstage/repository/CarInfoListRepository.java
@@ -1,6 +1,7 @@
 package com.spring_kajarta_frontstage.repository;
 
 import org.springframework.stereotype.Repository;
+import com.kajarta.demo.CarInfoList;
 
 @Repository
 public interface CarInfoListRepository extends JpaRepository<CarInfoList, Integer> {
