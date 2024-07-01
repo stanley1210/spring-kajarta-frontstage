dd34cc138608694afefe667770e8177e5421bdf2
Vic <g27942110n@gmail.com>
2024-07-01 22:40:16 +0800

    update repository

--- src/main/java/com/spring_kajarta_frontstage/repository/LikeListRepository.java
+++ src/main/java/com/spring_kajarta_frontstage/repository/LikeListRepository.java
@@ -1,6 +1,8 @@
 package com.spring_kajarta_frontstage.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.kajarta.demo.model.LikeList;
 
 @Repository
 public interface LikeListRepository extends JpaRepository<LikeList, Integer> {
