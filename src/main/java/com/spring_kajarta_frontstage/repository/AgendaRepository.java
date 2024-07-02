564d0e69406f15278e5db01c65cebdaafe543532
Enzo <d900124@gmail.com>
2024-07-02 18:12:55 +0800

    enzo 修改Repository

--- src/main/java/com/spring_kajarta_frontstage/repository/AgendaRepository.java
+++ src/main/java/com/spring_kajarta_frontstage/repository/AgendaRepository.java
@@ -1,7 +1,8 @@
 package com.spring_kajarta_frontstage.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.kajarta.demo.model.Agenda;
 
-public interface AgendaRepository extends JpaRepository {
+public interface AgendaRepository extends JpaRepository<Agenda, Integer> {
 
 }
--- src/main/java/com/spring_kajarta_frontstage/repository/CarAdjustRepository.java
+++ src/main/java/com/spring_kajarta_frontstage/repository/CarAdjustRepository.java
@@ -1,7 +1,8 @@
 package com.spring_kajarta_frontstage.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.kajarta.demo.model.CarAdjust;
 
-public interface CarAdjustRepository extends JpaRepository {
+public interface CarAdjustRepository extends JpaRepository<CarAdjust, Integer> {
 
 }
