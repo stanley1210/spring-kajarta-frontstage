621cd79b7fc80289ddfbd5615d57f4bf266fccd9
Vic <g27942110n@gmail.com>
2024-07-02 21:12:25 +0800

    update import model in CarRepository

--- src/main/java/com/spring_kajarta_frontstage/repository/CarRepository.java
+++ src/main/java/com/spring_kajarta_frontstage/repository/CarRepository.java
@@ -2,6 +2,8 @@ package com.spring_kajarta_frontstage.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.kajarta.demo.model.Car;
+
 public interface CarRepository extends JpaRepository<Car, Integer> {
 
 }
