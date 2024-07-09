2c4ad787077c2a89a521b87c92a110cee779ba26
sasha <yellow19950217@gmail.com>
2024-07-09 13:19:04 +0800

    edit Repository

--- src/main/java/com/spring_kajarta_frontstage/repository/CustomerRepository.java
+++ src/main/java/com/spring_kajarta_frontstage/repository/CustomerRepository.java
@@ -1,7 +1,9 @@
 package com.spring_kajarta_frontstage.repository;
 
+import com.kajarta.demo.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository {
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
--- src/main/java/com/spring_kajarta_frontstage/repository/LeaveRepository.java
+++ src/main/java/com/spring_kajarta_frontstage/repository/LeaveRepository.java
@@ -1,7 +1,9 @@
 package com.spring_kajarta_frontstage.repository;
 
+import com.kajarta.demo.model.Leave;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LeaveRepository {
+public interface LeaveRepository extends JpaRepository<Leave,Integer> {
 }
