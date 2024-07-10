89b41afed7aba7855223a4844948958e60b89d13
sasha <yellow19950217@gmail.com>
2024-07-10 11:03:11 +0800

    edit CustomerServiceImpl

--- src/main/java/com/spring_kajarta_frontstage/service/CustomerService.java
+++ src/main/java/com/spring_kajarta_frontstage/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.spring_kajarta_frontstage.service;
 import com.kajarta.demo.vo.CustomerVO;
 
 public interface CustomerService {
+
     // 單筆查詢，依據用戶id查詢單一用戶資訊
     CustomerVO findById(Integer customerId);
 }
