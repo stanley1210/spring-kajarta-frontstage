6ae405aca7ab034b918bbd2842e86985a423b847
stanley1210 <stbs1210@gmail.com>
2024-07-10 23:54:11 +0800

    ViewCarController select方法

--- src/main/java/com/spring_kajarta_frontstage/controller/ViewCarController.java
+++ src/main/java/com/spring_kajarta_frontstage/controller/ViewCarController.java
@@ -1,25 +1,33 @@
 package com.spring_kajarta_frontstage.controller;
 
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kajarta.demo.model.ViewCar;
+import com.spring_kajarta_frontstage.service.ViewCarService;
+import com.spring_kajarta_frontstage.util.DatetimeConverter;
+
 @RestController
-@RequestMapping("/kajarta/viewCar")
+@RequestMapping("/front/viewCar/create")
 @CrossOrigin
 public class ViewCarController {
+@Autowired
+private ViewCarService viewCarService;
 
-//  @PostMapping("/create")
+
+    //新增
+    //  @PostMapping("/create")
 //     public String create(@RequestBody String body) {
 //         JSONObject responseBody = new JSONObject();
-
 //         JSONObject obj = new JSONObject(body);
 //         Integer id = obj.isNull("id") ? null : obj.getInt("id");
-
 //         if(id==null) {
 //             responseBody.put("success", false);
 //             responseBody.put("message", "Id是必要欄位");
@@ -40,5 +48,45 @@ public class ViewCarController {
 //         }
 //         return responseBody.toString();
 //     }
+    //修改
+    //查一
+
+ @GetMapping("/front/viewCar/select")
+    public String findById(@PathVariable(name = "pk") Integer id) {
+        JSONObject responseBody = new JSONObject();
+        JSONArray array = new JSONArray();
+
+        ViewCar viewCar = viewCarService.findById(id);
+        if(viewCar!=null) {
+            String viewCarDate = DatetimeConverter.toString(viewCar.getViewCarDate(), "yyyy-MM-dd");
+            String createTime = DatetimeConverter.toString(viewCar.getCreateTime(), "yyyy-MM-dd");
+            String updateTime = DatetimeConverter.toString(viewCar.getUpdateTime(), "yyyy-MM-dd");
+            JSONObject item = new JSONObject()
+
+                    .put("id", viewCar.getId())
+                    .put("viewTimeSection", viewCar.getViewTimeSection())
+                    .put("car", viewCar.getCar())
+                    .put("salesScore", viewCar.getSalesScore())
+                    .put("factoryScore", viewCar.getFactoryScore())
+                    .put("viewCarDate",viewCarDate)
+                    .put("carScore", viewCar.getCarScore())
+                    .put("deal", viewCar.getDeal())
+                    .put("customer", viewCar.getCustomer())
+                    .put("createTime",createTime)
+                    .put("updateTime", updateTime)
+                    .put("viewCarStatus", viewCar.getViewCarStatus());
+            array = array.put(item);
+        }
+
+        responseBody.put("list", array);
+        return responseBody.toString();
+    }
+
+
+
+
+    //查全
+    //多條件查詢
+
 
 }
