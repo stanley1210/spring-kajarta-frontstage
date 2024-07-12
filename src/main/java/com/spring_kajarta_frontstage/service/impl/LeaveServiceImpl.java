901cec09fa91593e24addabc1911e4ab6999012f
sasha <yellow19950217@gmail.com>
2024-07-12 10:16:15 +0800

    edit leave service

--- src/main/java/com/spring_kajarta_frontstage/service/impl/LeaveServiceImpl.java
+++ src/main/java/com/spring_kajarta_frontstage/service/impl/LeaveServiceImpl.java
@@ -1,15 +1,16 @@
 package com.spring_kajarta_frontstage.service.impl;
 
-import com.kajarta.demo.model.Employee;
+
 import com.kajarta.demo.model.Leave;
-import com.spring_kajarta_frontstage.repository.EmployeeRepository;
 import com.spring_kajarta_frontstage.repository.LeaveRepository;
 import com.spring_kajarta_frontstage.service.LeaveService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 @Service
 public class LeaveServiceImpl implements LeaveService {
+    @Autowired
     private LeaveRepository leaveRepo;
     /**
      * 查詢單筆，依據請假id查詢單一請假資訊
