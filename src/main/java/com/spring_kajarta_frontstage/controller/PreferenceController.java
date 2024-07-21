74f02539506c9151351b794552b923252adaaeac
nick <gh3342002@hotmail.com.tw>
2024-07-21 12:21:32 +0800

    aaa

--- src/main/java/com/spring_kajarta_frontstage/controller/PreferenceController.java
+++ src/main/java/com/spring_kajarta_frontstage/controller/PreferenceController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.spring_kajarta_frontstage.service.CustomerService;
 import com.spring_kajarta_frontstage.service.PreferenceService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/preference")
 public class PreferenceController {
 
