e62a2c5422d88cd325e7fe7de56f3651c3d3aaba
nick <gh3342002@hotmail.com.tw>
2024-07-23 15:13:57 +0800

    新增brand finAll

--- src/main/java/com/spring_kajarta_frontstage/controller/EnumTableTestController.java
+++ src/main/java/com/spring_kajarta_frontstage/controller/EnumTableTestController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.DispatcherServlet;
 
-import com.kajarta.demo.model.Brand;
 import com.kajarta.demo.model.Displacement;
 import com.kajarta.demo.model.Door;
 import com.kajarta.demo.model.Gasoline;
@@ -34,9 +32,6 @@ import com.spring_kajarta_frontstage.service.TransmissionService;
 public class EnumTableTestController {
 
     @Autowired
-    private BrandService brandService;
-
-    @Autowired
     private NegotiableService negotiableService;
 
     @Autowired
@@ -59,15 +54,6 @@ public class EnumTableTestController {
 
     @Autowired
     private DisplacementService displacementService;
-
-    @GetMapping("/brand/{id}")
-    public String brand(@PathVariable(name = "id") Integer Id) {
-        Brand brand = brandService.findById(Id);
-        JSONObject responseBody = new JSONObject()
-                .put("brand", brand.getBrand())
-                .put("trademark", brand.getTrademark());
-        return responseBody.toString();
-    }
 
     @GetMapping("/negotiable/{id}")
     public String negotiable(@PathVariable(name = "id") Integer Id) {
--- src/main/java/com/spring_kajarta_frontstage/service/BrandService.java
+++ src/main/java/com/spring_kajarta_frontstage/service/BrandService.java
@@ -1,5 +1,7 @@
 package com.spring_kajarta_frontstage.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,14 @@ public class BrandService {
     @Autowired
     private BrandRepository brandRepo;
 
+    // 查單筆
     public Brand findById(Integer id) {
         return brandRepo.findById(id).get();
+    }
+
+    // 查全部
+    public List<Brand> findAll() {
+        return brandRepo.findAll();
     }
 
 }
