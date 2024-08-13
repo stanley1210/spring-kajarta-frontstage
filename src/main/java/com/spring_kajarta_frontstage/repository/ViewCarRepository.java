8d0b59ad11193f35dcef96108ce59169e4669290
stanley1210 <stbs1210@gmail.com>
2024-08-13 13:17:29 +0800

    123

--- src/main/java/com/spring_kajarta_frontstage/repository/ViewCarRepository.java
+++ src/main/java/com/spring_kajarta_frontstage/repository/ViewCarRepository.java
@@ -17,7 +17,7 @@ public interface ViewCarRepository extends JpaRepository<ViewCar, Integer> {
 	@Query(value = "from ViewCar")
 	List<ViewCar> find3Latest(Pageable pgb);
 
-	List<ViewCar> findByCustomer_Id(Integer customerId); // 根据 customerId 查找所有 ViewCar
+	List<ViewCar> findByCustomer_Id(Integer customerId); // 根據 customerId 查找所有 ViewCar
 
 	Page<ViewCar> findPageByCustomerId(Integer customerId, Pageable pageable);
 
@@ -27,5 +27,5 @@ public interface ViewCarRepository extends JpaRepository<ViewCar, Integer> {
 	@Query(value = "select view_car.* from view_car join view_car_assigned on view_car.id=view_car_assigned.view_car_id where employee_id=:employeeId And sales_score>=0", nativeQuery = true)
 	List<ViewCar> findViewCarByEmployeeId(Integer employeeId); // 搜尋銷售員負責的車
 
-	ViewCar findTopByOrderByIdDesc();//查詢最新ID
+	ViewCar findTopByOrderByIdDesc();// 查詢最新ID
 }
